decdbc2ae3eef4f78ec27229431aeeb5980802cf
JuanPiedra <juanda1103@hotmail.com>
2024-04-21 01:20:40 -0500

    update player multiplayer

--- Assets/Scripts/PlayerItem.cs
+++ Assets/Scripts/PlayerItem.cs
@@ -1,18 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+using Photon.Realtime;
+using TMPro;
+using System.Runtime.CompilerServices;
+
 
 public class PlayerItem : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public TextMeshProUGUI playerName;
+    Image backgroundImage;
+
+    public Color hightlightColor;
+    public GameObject leftArrowbtn;
+    public GameObject RightArrowBtn;
+
+    private void Start()
     {
-        
+        backgroundImage = GetComponent<Image>();    
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+
+
+    public void SetPlayerInfo(Player _Player) {
+
+        playerName.text = _Player.NickName;
+    
     }
+
+    public void ApplyLocalChanges()
+    {
+
+        backgroundImage.color = hightlightColor;
+        leftArrowbtn.SetActive(true);
+        RightArrowBtn.SetActive(false);
+
+
+    }
+
 }
